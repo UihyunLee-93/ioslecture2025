347674a8a96d3959d0e074b5eed0f3f0323efa0d
UihyunLee-93 <uihyun.dev@gmail.com>
2025-02-03 19:48:09 +0900

    0203 quiz

--- 02.Archive/Quiz/25-02-week1/02-03.swift
+++ 02.Archive/Quiz/25-02-week1/02-03.swift
@@ -1,1 +1,50 @@
-import foun
+import Foundation
+
+let MAX = 21
+
+var prev = 0
+var cur = 1
+var next = 1
+var total = 0
+var output = "\(cur)"
+
+total += cur
+
+while true {
+    next = prev + cur
+    if next > MAX { break }
+    
+    output += " + \(next)"
+    total += next
+
+    prev = cur
+    cur = next
+}
+
+print("일반 피보나치 수열")
+print("\(output) = \(total)\n")
+
+prev = 0
+cur = 1
+next = 1
+total = 0
+output = "\(cur)"
+var flag = false  
+
+total += cur
+
+while true {
+    next = prev + cur
+    if next > MAX { break }
+
+    let sign = flag ? "-" : "+"
+    output += " \(sign) \(next)"
+    total += flag ? -next : next
+
+    prev = cur
+    cur = next
+    flag.toggle()
+}
+
+print("반전된 피보나치 수열")
+print("\(output) = \(total)")
