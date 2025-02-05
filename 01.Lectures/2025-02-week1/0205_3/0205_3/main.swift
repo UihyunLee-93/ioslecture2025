59b7e3bbd1d033b1585054935aea59667d2e038b
UihyunLee-93 <uihyun.dev@gmail.com>
2025-02-05 18:31:24 +0900

    0205

--- 01.Lectures/2025-02-week1/0205_3/0205_3/main.swift
+++ 01.Lectures/2025-02-week1/0205_3/0205_3/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+//func myFunc(){
+//    print ("호출 myFunc")
+//}
+//myFunc()
+//
+//var refFunc = myFunc
 
+
+//클로저
+
+//func mult (a: Int, b: Int)->Int{
+//    return a*b
+//}
+//let multiply = {(a: Int,b: Int)->Int in a * b}
+//print(multiply(10,20))
+
+let numbers: [Int] = [4,5,2,1,3]
+
+let newNumbers: [Int] = numbers.map{ $0 * 2}
+print(newNumbers)
+    
+let sortedArr: [Int] = numbers.sorted{ $0 < $1 }
+print(sortedArr)
