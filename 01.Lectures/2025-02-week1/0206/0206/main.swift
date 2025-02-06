68c0c3fb3fde2bb394bde8ea5cfb8d9a5b2d7aae
UihyunLee-93 <uihyun.dev@gmail.com>
2025-02-06 13:10:08 +0900

    실습문제

---
+++ 01.Lectures/2025-02-week1/0206/0206/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  0206
+//
+//  Created by Uihyun.Lee on 2/6/25.
+//
+
+import Foundation
+
+
+
+let myCar = Car(color: "red", speed: 100)
+let sonata = Car(color: "green", speed: 200)
+let corolla = Car(color: "blue", speed: 150)
+
+
---
+++ 01.Lectures/2025-02-week1/0206_2/0206_2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  0206_2
+//
+//  Created by Uihyun.Lee on 2/6/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
