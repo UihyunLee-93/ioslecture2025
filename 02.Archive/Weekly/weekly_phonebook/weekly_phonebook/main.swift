848f6de85595966fa4aedfcb467591e1303ac0b8
UihyunLee-93 <uihyun.dev@gmail.com>
2025-02-07 16:29:18 +0900

    commit

--- 02.Archive/Weekly/weekly_phonebook/weekly_phonebook/main.swift
+++ 02.Archive/Weekly/weekly_phonebook/weekly_phonebook/main.swift
@@ -1,11 +1,18 @@
-//
-//  main.swift
-//  weekly_phonebook
-//
-//  Created by Uihyun.Lee on 2/7/25.
-//
-
 import Foundation
 
-print("Hello, World!")
+let phoneBookManager = PhoneBookManager()
 
+func main() {
+    while true {
+        phoneBookManager.menu()
+        print("번호를 선택하세요:", terminator: " ")
+        
+        if let choice = readLine(), let option = Int(choice) {
+            phoneBookManager.menuSelection(option) 
+        } else {
+            print(" 잘못된 입력입니다. 숫자를 입력하세요.")
+        }
+    }
+}
+
+main()
