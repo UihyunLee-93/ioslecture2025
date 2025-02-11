d40e508011dbffdef7f616949badc566c01991a5
UihyunLee-93 <uihyun.dev@gmail.com>
2025-02-11 22:53:09 +0900

    commit

---
+++ Project/SampleProjectQuiz/SampleProjectQuiz/SampleProjectQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectQuizApp.swift
+//  SampleProjectQuiz
+//
+//  Created by Uihyun.Lee on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project/SampleProjectQuiz/SampleProjectQuizTests/SampleProjectQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectQuizTests.swift
+//  SampleProjectQuizTests
+//
+//  Created by Uihyun.Lee on 2/11/25.
+//
+
+import Testing
+@testable import SampleProjectQuiz
+
+struct SampleProjectQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
